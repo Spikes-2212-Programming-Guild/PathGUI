1c5fe00621931f05aa570c96060e016f608283c8
Ergoold <eran1gold@gmail.com>
2020-01-18 15:49:18 +0200

    added `Constants` data class

---
+++ src/main/java/gui/Constants.java
@@ -0,0 +1,43 @@
+package gui;
+
+public class Constants {
+    private double spacing;
+    private double smoothWeight;
+    private double tolerance;
+    private double maxVelocity;
+    private double turningConstant;
+    private double maxAcceleration;
+
+    public Constants(double spacing, double smoothWeight, double tolerance, double maxVelocity, double turningConstant, double maxAcceleration) {
+        this.spacing = spacing;
+        this.smoothWeight = smoothWeight;
+        this.tolerance = tolerance;
+        this.maxVelocity = maxVelocity;
+        this.turningConstant = turningConstant;
+        this.maxAcceleration = maxAcceleration;
+    }
+
+    public double getSpacing() {
+        return spacing;
+    }
+
+    public double getSmoothWeight() {
+        return smoothWeight;
+    }
+
+    public double getTolerance() {
+        return tolerance;
+    }
+
+    public double getMaxVelocity() {
+        return maxVelocity;
+    }
+
+    public double getTurningConstant() {
+        return turningConstant;
+    }
+
+    public double getMaxAcceleration() {
+        return maxAcceleration;
+    }
+}
