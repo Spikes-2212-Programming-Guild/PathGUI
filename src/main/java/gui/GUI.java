810bab74ad386fad0686530242a570f3671d8ccc
Ergoold <eran1gold@gmail.com>
2020-01-18 12:19:34 +0200

    Added `GUI`

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -5,20 +5,16 @@ import java.awt.*;
 import java.nio.file.Paths;
 
 public class GUI extends JFrame {
-    public static void main(String[] args)  {
+    public GUI() {
+        setContentPane(new PathViewer(Paths.get("D:/paths/path.csv")));
+    }
+
+    public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setPreferredSize(new Dimension(1598, 821));
         gui.setResizable(false);
         gui.pack();
         gui.setVisible(true);
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public GUI() {
-        try {
-            setContentPane(new PathViewer(Paths.get("D:/paths/path.csv")));
-        } catch(Exception e) {
-            JOptionPane.showMessageDialog(this, e.toString(), "Oops", JOptionPane.ERROR_MESSAGE);
-        }
     }
 }
