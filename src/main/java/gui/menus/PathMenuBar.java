35b496fb5d3506455757d6531d5e30da0bed5de5
Ergoold <eran1gold@gmail.com>
2020-01-21 08:51:07 +0200

    added keyboard shortcuts

--- src/main/java/gui/menus/PathMenuBar.java
+++ src/main/java/gui/menus/PathMenuBar.java
@@ -3,6 +3,7 @@ package gui.menus;
 import gui.GUI;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class PathMenuBar extends JMenuBar {
     public PathMenuBar(GUI gui) {
@@ -10,16 +11,19 @@ public class PathMenuBar extends JMenuBar {
         file.setMnemonic('F');
 
         JMenuItem newFile = new JMenuItem("New");
-        newFile.setMnemonic('N');
         newFile.addActionListener(new NewListener(gui));
+        newFile.setMnemonic('N');
+        newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
 
         JMenuItem open = new JMenuItem("Open");
         open.setMnemonic('O');
         open.addActionListener(new OpenListener(gui));
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
 
         JMenuItem save = new JMenuItem("Save As");
-        save.setMnemonic('S');
         save.addActionListener(new SaveListener(gui));
+        save.setMnemonic('S');
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
 
         file.add(newFile);
         file.add(open);
@@ -31,12 +35,13 @@ public class PathMenuBar extends JMenuBar {
         edit.setMnemonic('E');
 
         JMenuItem mirror = new JMenuItem("Mirror Path");
-        mirror.setMnemonic('M');
         mirror.addActionListener(new MirrorListener(gui));
+        mirror.setMnemonic('M');
 
         JMenuItem undo = new JMenuItem("Undo");
-        undo.setMnemonic('U');
         undo.addActionListener(new UndoListener(gui));
+        undo.setMnemonic('U');
+        undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK));
 
         edit.add(undo);
         edit.addSeparator();
