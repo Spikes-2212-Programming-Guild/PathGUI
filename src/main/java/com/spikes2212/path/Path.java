72f177e21fdb5b73751d8b2e938bf0259beb8a5a
Ergoold <eran1gold@gmail.com>
2020-01-26 12:19:06 +0200

    reformat `Path.java`

--- src/main/java/com/spikes2212/path/Path.java
+++ src/main/java/com/spikes2212/path/Path.java
@@ -16,14 +16,15 @@ public class Path {
 
     /**
      * Initializes a path.
-     * @param spacing the distance between two points filled in between points given as parameters
-     * @param smooth_weight how smooth to make the path
-     * @param tolerance the smoothing tolerance
-     * @param maxVelocity the robot's maximum velocity
+     *
+     * @param spacing         the distance between two points filled in between points given as parameters
+     * @param smooth_weight   how smooth to make the path
+     * @param tolerance       the smoothing tolerance
+     * @param maxVelocity     the robot's maximum velocity
      * @param turningConstant speed constant at curves (the higher it is, the faster you turn)
      * @param maxAcceleration the robot's maximum acceleration
-     * @param points the initial points on the path. Apart from the edges, non of the points are guaranteed
-     *               to be on the final path
+     * @param points          the initial points on the path. Apart from the edges, non of the points are guaranteed
+     *                        to be on the final path
      */
     public Path(double spacing, double smooth_weight, double tolerance, double maxVelocity, double turningConstant,
                 double maxAcceleration, Waypoint... points) {
@@ -60,7 +61,7 @@ public class Path {
     }
 
     public void generate(double spacing, double smooth_weight, double tolerance,
-                          double maxVelocity, double k, double maxAcceleration) {
+                         double maxVelocity, double k, double maxAcceleration) {
         fill(spacing);
         smooth(smooth_weight, tolerance);
         calculateDistances();
@@ -70,16 +71,16 @@ public class Path {
     }
 
     private void fill(double spacing) {
-        for (int i = 0; i < points.size() - 1; i++) {
+        for(int i = 0; i < points.size() - 1; i++) {
             Waypoint startPoint = points.get(i);
-            double length = points.get(i).distance(points.get(i+1));
-            int pointsThatFit = (int) (length/spacing);
-            Waypoint vector = new Waypoint((points.get(i+1).getX() - points.get(i).getX()) * (spacing/length),
-                    (points.get(i+1).getY() - points.get(i).getY()) * (spacing/length));
-            for (int j = 0; j < pointsThatFit; j++, i++) {
+            double length = points.get(i).distance(points.get(i + 1));
+            int pointsThatFit = (int)(length / spacing);
+            Waypoint vector = new Waypoint((points.get(i + 1).getX() - points.get(i).getX()) * (spacing / length),
+                    (points.get(i + 1).getY() - points.get(i).getY()) * (spacing / length));
+            for(int j = 0; j < pointsThatFit; j++, i++) {
                 points.add(i + 1, new Waypoint(
-                        startPoint.getX() + vector.getX()*(j+1),
-                        startPoint.getY() + vector.getY()*(j+1)
+                        startPoint.getX() + vector.getX() * (j + 1),
+                        startPoint.getY() + vector.getY() * (j + 1)
                 ));
             }
         }
@@ -87,25 +88,25 @@ public class Path {
 
     private void smooth(double smooth_weight, double tolerance) {
         double data_weight = 1 - smooth_weight;
-        double [][] path = new double[points.size()][2];
-        for (int i = 0; i < points.size(); i++) {
+        double[][] path = new double[points.size()][2];
+        for(int i = 0; i < points.size(); i++) {
             path[i] = points.get(i).toArray();
         }
-        double [][] ogPath = Arrays.copyOf(path, path.length);
+        double[][] ogPath = Arrays.copyOf(path, path.length);
         double change = tolerance;
-        while (change >= tolerance) {
+        while(change >= tolerance) {
             change = 0;
             for(int i = 1; i < path.length - 1; i++) {
                 for(int j = 0; j < path[i].length; j++) {
                     double aux = path[i][j];
                     path[i][j] += data_weight * (ogPath[i][j] - path[i][j])
-                            + smooth_weight * (path[i-1][j] + path[i+1][j] - 2 * path[i][j]);
+                            + smooth_weight * (path[i - 1][j] + path[i + 1][j] - 2 * path[i][j]);
                     change += Math.abs(aux - path[i][j]);
                 }
             }
         }
 
-        for (int i = 0; i < path.length; i++) {
+        for(int i = 0; i < path.length; i++) {
             points.set(i, new Waypoint(path[i][0], path[i][1]));
         }
     }
@@ -113,8 +114,8 @@ public class Path {
     private void calculateDistances() {
         double previousDistance = 0;
         points.get(0).setD(0);
-        for (int i = 1; i < points.size(); i++) {
-            previousDistance += points.get(i).distance(points.get(i-1));
+        for(int i = 1; i < points.size(); i++) {
+            previousDistance += points.get(i).distance(points.get(i - 1));
             points.get(i).setD(previousDistance);
         }
     }
@@ -128,28 +129,28 @@ public class Path {
             double x3 = points.get(i + 1).getX();
             double y3 = points.get(i + 1).getY();
             if(x1 == x2) x2 += 0.000001;
-            double k1 = 0.5 * (x1*x1 + y1*y1 - x2*x2 - y2*y2) / (x1 - x2);
+            double k1 = 0.5 * (x1 * x1 + y1 * y1 - x2 * x2 - y2 * y2) / (x1 - x2);
             double k2 = (y1 - y2) / (x1 - x2);
-            double b = 0.5 * (x2*x2 - 2*x2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3) / (x3*k2 - y3 + y2 - x2*k2);
+            double b = 0.5 * (x2 * x2 - 2 * x2 * k1 + y2 * y2 - x3 * x3 + 2 * x3 * k1 - y3 * y3) / (x3 * k2 - y3 + y2 - x2 * k2);
             double a = k1 - k2 * b;
-            double r = Math.sqrt((x1-a)*(x1-a) + (y1-b)*(y1-b));
-            points.get(i).setCurvature(1/r);
+            double r = Math.sqrt((x1 - a) * (x1 - a) + (y1 - b) * (y1 - b));
+            points.get(i).setCurvature(1 / r);
         }
     }
 
     private void calculateMaxVelocities(double maxVelocity, double k) {
-        for (Waypoint p : points) {
-            p.setV(Math.min(maxVelocity, k/p.getCurvature()));
+        for(Waypoint p : points) {
+            p.setV(Math.min(maxVelocity, k / p.getCurvature()));
         }
     }
 
     private void smoothVelocities(double maxAcceleration) {
         points.get(points.size() - 1).setV(0);
-        for (int i = points.size() - 2; i >= 0; i--) {
+        for(int i = points.size() - 2; i >= 0; i--) {
             double distance;
-            distance = points.get(i).distance(points.get(i+1));
+            distance = points.get(i).distance(points.get(i + 1));
             points.get(i).setV(Math.min(points.get(i).getV(),
-                    Math.sqrt(Math.pow(points.get(i+1).getV(), 2) + 2*maxAcceleration*distance)));
+                    Math.sqrt(Math.pow(points.get(i + 1).getV(), 2) + 2 * maxAcceleration * distance)));
         }
     }
 }
