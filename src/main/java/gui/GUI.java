68c398f8fefa64774c3e3ff823b1d94f7bc100e4
Ergoold <eran1gold@gmail.com>
2020-01-26 12:42:37 +0200

    add documentation for `GUI`

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -6,8 +6,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.prefs.Preferences;
 
+/**
+ * The main frame that holds the paths GUI.
+ */
 public class GUI extends JFrame {
+    /**
+     * The file chooser used when opening and saving path files.
+     *
+     * Having one global file chooser allows us to keep directory preferences without constant accesses to the
+     * Preferences object.
+     */
     private JFileChooser fileChooser;
+
+    /**
+     * The preferences object that saves the preferences for default directory and path generation gains.
+     */
     private Preferences preferences;
 
     public GUI() {
