7b06e8b75d3efa47ff7612564c4b2e5554f0c75e
Ergoold <eran1gold@gmail.com>
2020-02-15 18:59:19 +0200

    add documentation in `PathToolBar`

--- src/main/java/gui/toolbar/PathToolBar.java
+++ src/main/java/gui/toolbar/PathToolBar.java
@@ -6,10 +6,25 @@ import gui.PathMaker;
 import javax.swing.*;
 import java.text.NumberFormat;
 
+/**
+ * The tool bar for the path GUI.
+ */
 public class PathToolBar extends JToolBar {
+    /**
+     * The text field input for the x coordinate.
+     */
     private JFormattedTextField xField;
+
+    /**
+     * The text field input for the x coordinate.
+     */
     private JFormattedTextField yField;
 
+    /**
+     * Sets up the tool bar, menus and menu items.
+     *
+     * @param pathMaker a path drawing interface object.
+     */
     public PathToolBar(PathMaker pathMaker) {
         super("Tool Bar | Spikes Path Drawing Tool");
 
@@ -33,6 +48,12 @@ public class PathToolBar extends JToolBar {
         add(update);
     }
 
+    /**
+     * Set the coordinates displayed on the tool bar's text fields.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public void setCoordinates(int x, int y) {
         xField.setText(String.valueOf(x));
         yField.setText(String.valueOf(y));
