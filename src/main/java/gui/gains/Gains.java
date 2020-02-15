c372a3755b5e410d80c2b188d4cc61b6f9307871
Ergoold <eran1gold@gmail.com>
2020-02-15 18:44:01 +0200

    correct documentation in `CloseListener`

--- src/main/java/gui/gains/Gains.java
+++ src/main/java/gui/gains/Gains.java
@@ -2,12 +2,38 @@ package gui.gains;
 
 import gui.GUI;
 
+/**
+ * The gains for the path generation algorithm.
+ */
 public class Gains {
+    /**
+     * The spacing between points on the generated path.
+     */
     private double spacing;
+
+    /**
+     * How much to smooth the path.
+     */
     private double smoothWeight;
+
+    /**
+     * The highest tolerable distance between the smoothed and original paths.
+     */
     private double tolerance;
+
+    /**
+     * The maximum velocity of the robot.
+     */
     private double maxVelocity;
+
+    /**
+     * The turning constant for the path generation algorithm.
+     */
     private double turningConstant;
+
+    /**
+     * The maximum acceleration of the robot.
+     */
     private double maxAcceleration;
 
     public Gains(double spacing, double smoothWeight, double tolerance, double maxVelocity, double turningConstant, double maxAcceleration) {
@@ -43,6 +69,12 @@ public class Gains {
         return maxAcceleration;
     }
 
+    /**
+     * Returns the user-saved default gains.
+     *
+     * @param gui a program object that holds the user preferences
+     * @return the user-saved default gains
+     */
     public static Gains getPreferences(GUI gui) {
         return new Gains(Double.parseDouble(gui.getPreferences().get("SPACING", "0.075")),
                 Double.parseDouble(gui.getPreferences().get("SMOOTH_WEIGHT", "0.6")),
