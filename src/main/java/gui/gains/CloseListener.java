fd17703761dddf723b500913685f088ffa673a7a
Ergoold <eran1gold@gmail.com>
2020-02-15 18:36:28 +0200

    add documentation to `CloseListener`

--- src/main/java/gui/gains/CloseListener.java
+++ src/main/java/gui/gains/CloseListener.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * An action that closes a dialog.
+ */
 public class CloseListener implements ActionListener {
+    /**
+     * The dialog to close.
+     */
     JDialog dialog;
 
     public CloseListener(JDialog dialog) {
