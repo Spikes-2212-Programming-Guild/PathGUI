31b2fbdba7c067f1dd1498fece5efde450530743
Ergoold <eran1gold@gmail.com>
2020-02-19 18:17:48 +0200

    add icons to menus

--- src/main/java/gui/menus/PathMenuBar.java
+++ src/main/java/gui/menus/PathMenuBar.java
@@ -21,20 +21,20 @@ public class PathMenuBar extends JMenuBar {
         JMenu file = new JMenu("File");
         file.setMnemonic('F');
 
-        JMenuItem newFile = new JMenuItem("New");
+        JMenuItem newFile = new JMenuItem("New", new ImageIcon(getClass().getResource("/res/icon/new.png")));
         newFile.addActionListener(new NewListener(gui));
         newFile.setMnemonic('N');
         newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
 
-        JMenuItem open = new JMenuItem("Open");
+        JMenuItem open = new JMenuItem("Open", new ImageIcon(getClass().getResource("/res/icon/open.png")));
         open.setMnemonic('O');
         open.addActionListener(new OpenListener(gui));
         open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
 
-        JMenuItem saveAs = new JMenuItem("Save As");
-        saveAs.addActionListener(new SaveAsListener(gui));
-        saveAs.setMnemonic('A');
-        saveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.SHIFT_DOWN_MASK));
+        JMenuItem save = new JMenuItem("Save", new ImageIcon(getClass().getResource("/res/icon/save.png")));
+        save.addActionListener(new SaveAsListener(gui));
+        save.setMnemonic('S');
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
 
         JMenuItem export = new JMenuItem("Export");
         export.addActionListener(new ExportListener(gui));
@@ -44,14 +44,14 @@ public class PathMenuBar extends JMenuBar {
         file.add(newFile);
         file.add(open);
         file.addSeparator();
-        file.add(saveAs);
+        file.add(save);
         file.add(export);
         add(file);
 
         JMenu edit = new JMenu("Edit");
         edit.setMnemonic('E');
 
-        JMenuItem undo = new JMenuItem("Undo");
+        JMenuItem undo = new JMenuItem("Undo", new ImageIcon(getClass().getResource("/res/icon/undo.png")));
         undo.addActionListener(new UndoListener(gui));
         undo.setMnemonic('U');
         undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK));
@@ -66,7 +66,8 @@ public class PathMenuBar extends JMenuBar {
         quickGeneration.setMnemonic('Q');
         quickGeneration.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, KeyEvent.CTRL_DOWN_MASK));
 
-        JMenuItem mirror = new JMenuItem("Mirror Path");
+        JMenuItem mirror = new JMenuItem("Mirror Path",
+                new ImageIcon(getClass().getResource("/res/icon/mirror.png")));
         mirror.addActionListener(new MirrorListener(gui));
         mirror.setMnemonic('M');
 
