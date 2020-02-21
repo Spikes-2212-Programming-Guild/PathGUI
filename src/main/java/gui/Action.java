a22fdc40a61f8698662c85ae62d06b68ca3db3bc
Ergoold <eran1gold@gmail.com>
2020-02-21 15:16:33 +0200

    add javadoc to `Action`

--- src/main/java/gui/Action.java
+++ src/main/java/gui/Action.java
@@ -2,12 +2,27 @@ package gui;
 
 import path.Waypoint;
 
+/**
+ * An action performed by the user.
+ *
+ * @author Eran Goldstein
+ */
 public class Action {
+    /**
+     * Types of actions the user may perform.
+     */
     public enum ActionType {
         ADD, MIRROR, GENERATE
     }
 
+    /**
+     * The type of the action.
+     */
     private ActionType type;
+
+    /**
+     * The destination the action was directed towards.
+     */
     private Waypoint to;
 
     public Action(ActionType type, Waypoint to) {
@@ -15,8 +30,11 @@ public class Action {
         this.to = to;
     }
 
+    /**
+     * Constructor for actions with no destination.
+     */
     public Action(ActionType type) {
-        this(type, new Waypoint(0, 0));
+        this(type, null);
     }
 
     public ActionType getType() {
