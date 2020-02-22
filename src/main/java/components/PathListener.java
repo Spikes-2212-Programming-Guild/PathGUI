d96c57f3be3283d66b66580245625b6d079f029c
Ergoold <eran1gold@gmail.com>
2020-02-22 22:19:22 +0200

    expand javadoc in `PathListener`

--- src/main/java/components/PathListener.java
+++ src/main/java/components/PathListener.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 import java.util.Stack;
 
 /**
- * An {@link ActionListener} that pushes the action into the {@code UNDO_STACK}.
+ * An {@link ActionListener} that pushes the action into the {@code UNDO_STACK} and repaints the path pane.
+ *
+ * @author Eran Goldstein
  */
 public abstract class PathListener implements ActionListener {
     /**
